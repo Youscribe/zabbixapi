52333f34e837f65c2acb349c7b1772bbbf38be5d
Hendrik F. Louw <hendrik@qualica.com>
2011-08-29 09:15:14 +0200

    add basic history functionality

---
+++ lib/zabbixapi/history.rb
@@ -0,0 +1,45 @@
+module Zabbix
+
+  class ZabbixApi
+    #http://www.zabbix.com/documentation/1.8/api/history/get
+    def get_history(options)
+      defaults = {
+        'history' => [],
+        'nodeids' => [],
+        'hostids' => [],
+        'triggerids' => [],
+        'itemids' => [],
+        'time_from' => 0,
+        'time_till' => 10,
+        'filter' => [],
+        'pattern' => '',
+        'startPattern' => 0,
+        'excludePattern' => 0,
+        'searchWildcardsEnabled' => 0,
+        'output' => '',  #shorten, refer, extendoutput
+        'sortfield' => '', #itemid, clock
+        'sortorder' => '', # ASC, DESC
+        'limit' => 10000
+      }
+      
+      merge_opt(defaults, options)
+      
+      message = {
+        'method' => 'history.get',
+        'params' => {
+          params
+        }
+      }
+
+      response = send_request(message)
+
+      unless ( response.empty? ) then
+        result = response
+      else
+        result = nil
+      end
+      return result
+    end
+    
+  end
+end
